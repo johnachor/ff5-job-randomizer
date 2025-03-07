ceb01717f092429a59c0f30bbc783c7d53bd304e
johnachor <johnachor@gmail.com>
2025-03-07 17:26:09 -0600

    apis should like, respond, or something

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 
+const basicGet = (req, res, next) => {
+    res.send("It works bitch");
+};
+
+app.get("/", basicGet);
+
 app.listen(42069, () => {
     console.log("Server running on port 42069")
 });
