6f5d6a0cf5a11e0b73a9335267f963fb2264d9c9
johnachor <johnachor@gmail.com>
2025-03-07 18:19:21 -0600

    state management??

--- index.js
+++ index.js
@@ -11,19 +11,28 @@ const basicGet = (req, res, next) => {
     res.send(`Randomization time! Change ${chosenCharacter} to a ${chosenJob}!`);
 };
 
+const getAvailableJob = (req, res, next) => {
+    res.send(`Available jobs: ${jobSets.slice(0,unlockState).flat()}`)
+};
+
 const resetProgression = (req, res, next) => {
     unlockState = 0;
     res.send("Game progression has been reset. Let's go!")
 };
 
 const progressGame = (req, res, next) => {
-    unlockState = unlockState + 1;
-    res.send(`Progress logged! You've unlocked ${jobSets[unlockState]} new jobs!`);
+    if (unlockState >= 6) {
+        res.send("Progression is already maxed. Use !resetjobs to start over.");
+    } else {
+        unlockState = unlockState + 1;
+        res.send(`Progress logged! You've unlocked ${jobSets[unlockState]} new jobs!`);
+    }
 };
 
 app.get("/", basicGet);
 app.get("/reset", resetProgression);
 app.get("/unlock", progressGame);
+app.get("/available", getAvailableJob);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
