3729dc098266bff58322279c028f47c5e5b23f86
johnachor <johnachor@gmail.com>
2025-03-07 18:42:24 -0600

    allow freelancer toggle

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get("/", basicGet);
 app.get("/reset", resetProgression);
 app.get("/unlock", progressGame);
 app.get("/available", getAvailableJob);
+app.get("/freelancer", toggleFreelancer);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
